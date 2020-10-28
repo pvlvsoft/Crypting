6647be341b03fd5773a7d8fcf920a58b2f9fd747
vojta <vojta@pavlu.eu>
2020-10-28 16:15:55 +0100

    Project initialization

---
+++ src/main/java/pvlvsoft/App.java
@@ -0,0 +1,13 @@
+package pvlvsoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
